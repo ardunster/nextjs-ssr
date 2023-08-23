40eae4befbe90a21e60d0a741e80fcab554c5385
Anna Dunster <anna@annardunster.com>
2023-08-23 13:50:50 -0700

    Remove unnecessary passHref attribute

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 function Navbar() {
   return (
     <nav className={'navbar'}>
-      <Link href="/" passHref>
+      <Link href={'/'}>
         <h2>Main Page</h2>
       </Link>
-      <Link href="/articles" passHref>
+      <Link href={'/articles'}>
         <h3>Articles</h3>
       </Link>
     </nav>

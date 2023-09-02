5d491aefe80484d36acf20c1997550d9231b2c7d
Anna Dunster <anna@annardunster.com>
2023-09-02 13:49:24 -0700

    Add e2e tests for happy path (existing) article

---
+++ e2e/articles/article.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('single article page', () => {
+  test.beforeEach(async ({ page }) => {
+    // Test verify against a specific article, will need to be updated when real articles
+    // replace mocks used to develop code.
+    await page.goto('/articles/article2')
+  })
+
+  test('exists', async ({ page }) => {
+    await expect(page.locator('h1')).toHaveText('Older Test Article')
+  })
+
+  test('contains breadcrumbs', async ({ page }) => {
+    await expect(page.getByText('breadcrumb')).toBeVisible()
+  })
+
+  test('contains created date', async ({ page }) => {
+    await expect(page.getByText('created')).toBeVisible()
+  })
+
+  test('contains thumbnail image', async ({ page }) => {
+    await expect(page.getByAltText('thumbnail')).toBeVisible()
+  })
+})

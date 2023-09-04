f2db064148a1b17bfca1c0bd9fca52206634d9b6
Anna Dunster <anna@annardunster.com>
2023-09-04 15:12:11 -0700

    Update NextJS to resolve layout bugs, clean up troubleshooting code

--- src/app/articles/layout.tsx
+++ src/app/articles/layout.tsx
@@ -2,12 +2,9 @@ import { ReactNode } from 'react'
 import { Breadcrumbs } from '@/app/_components/client/Breadcrumbs'
 
 export default function ArticleLayout({ children }: { children: ReactNode }) {
-  console.log('in article layout')
   return (
     <>
       <Breadcrumbs />
-      Article level layout is here.
-      <br />
       {children}
     </>
   )
--- src/app/articles/not-found.tsx
+++ src/app/articles/not-found.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link'
-import { Breadcrumbs } from '@/app/_components/client/Breadcrumbs'
 
 export default function ArticleNotFoundPage() {
   return (
     <>
       <h1>Article Not Found.</h1>
-      <Breadcrumbs />
       Custom error recovery such as links back to the{' '}
       <Link href={'/'}>Main Page</Link> or back to the{' '}
       <Link href={'/articles'}>Article directory</Link> go here.

67c48d47356ccce947725ba4afdc2aa6d33e88c3
Anna Dunster <anna@annardunster.com>
2023-12-23 13:34:26 -0800

    Remove unnecessary experimental app dir config

--- next.config.js
+++ next.config.js
@@ -12,9 +12,6 @@ const withMDX = WithMDX({
 const nextConfig = {
   // Configure pageExtensions to include md and mdx
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  experimental: {
-    appDir: true,
-  },
   // Optionally, add any other Next.js config below
   reactStrictMode: true,
 }

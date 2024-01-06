ee55b5c210f9df5e18c6b90195c5a546f2166e7a
Anna Dunster <anna@annardunster.com>
2024-01-06 14:30:11 -0800

    Reorder functions in tags to match tutorial build order

--- src/app/_utils/tags.ts
+++ src/app/_utils/tags.ts
@@ -2,18 +2,6 @@ import { Article } from '@/app/_utils/articles'
 
 export type TagCount = { tag: string; count: number }
 
-export function aggregateTags(articles: Article[]) {
-  const tags: TagCount[] = []
-
-  for (const article of articles) {
-    if (article.data.tags && article.data.tags.length > 0) {
-      updateTagCounts(tags, article.data.tags)
-    }
-  }
-
-  return tags.sort(sortTagCounts)
-}
-
 export function updateTagCounts(tags: TagCount[], newTags: string[]) {
   for (const newTag of newTags) {
     const existingTag = tags.find((tag) => tag.tag === newTag)
@@ -28,6 +16,17 @@ export function updateTagCounts(tags: TagCount[], newTags: string[]) {
 /** Sorts descending with highest quantity first. */
 export function sortTagCounts(tagA: TagCount, tagB: TagCount): number {
   return tagB.count - tagA.count
+}
+
+export function aggregateTags(articles: Article[]) {
+  const tags: TagCount[] = []
+
+  for (const article of articles) {
+    if (article.data.tags && article.data.tags.length > 0) {
+      updateTagCounts(tags, article.data.tags)
+    }
+  }
+  return tags.sort(sortTagCounts)
 }
 
 export function filterArticlesByTag(
